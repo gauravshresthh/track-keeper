3897d7bfc150480aba73a5977aae336061a87b53
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-10-26 15:34:50 +0545

    Mongodb instance setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+const mongoURI = 'mongodb+srv://gauravshresthh:pikachhu@tracker.hpgdy.mongodb.net/tracker?retryWrites=true&w=majority'
+
+
+mongoose.connect(mongoURI,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+})
+
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to Mongo Instance');
+});
+
+mongoose.connection.on('error', err =>{
+    console.error('Error connecting to mongo', err);
+});
+
 
 app.get('/',(req,res) =>{
     res.send('Hi there')
